efb98315ddbba6ddc5aff9954995353bba9071c0
Kwakcena <rhkrgudwh@gmail.com>
2022-04-02 00:37:03 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(path.resolve(), 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'session-cookie',
+}));
+
+app.use((req, res, next) => {
+  console.log('모든 요청에 다 실행됩니다.');
+  next();
+});
+
+app.get('/', (req, res, next) => {
+  console.log('GET / 요청에서만 실행됩니다.');
+  next();
+}, (req, res) => {
+  throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
