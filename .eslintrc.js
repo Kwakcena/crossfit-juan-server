b20e88b803d3ec2a50a524cfe9e6df0442bb4dd7
Kwakcena <rhkrgudwh@gmail.com>
2022-04-03 04:49:09 +0900

    Add eslint rule
    eslint를 설치하고 rule을 추가합니다.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es2021: true,
+    jest: true,
   },
   extends: [
     'airbnb-base',
   ],
+  globals: {
+    context: 'readonly',
+  },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
+    semi: ['error'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
   },
 };
